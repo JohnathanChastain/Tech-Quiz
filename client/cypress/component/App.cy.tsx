636b3af7274e7f12145c76c889727d3cb7d90d77
Johnathan Chastain <chastainjohnathan75@gmail.com>
2025-01-12 14:21:58 -0600

    added file structure

--- client/cypress/component/App.cy.tsx
+++
@@ -1,9 +0,0 @@
-import { mount } from 'cypress/react';
-import App from '../../src/App';
-
-describe('App Component', () => {
-  it('renders correctly', () => {
-    mount(<App />);
-    cy.get('h1').should('contain', 'Welcome to the App');
-  });
-});
